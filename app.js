12f3bfb50b040e5d062c0ec280f4fd6f674218a2
Lance Tse <steam.bao666@gmail.com>
2023-12-08 23:05:19 -0800

    "Feature 3 complete"

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//guess counter, increment when guessOnce is called
+let guessCount = 0;
+
 //prompt for and store input value
 let guessOnce = function guessOnce() {
-console.log("\tEntering f: guessOnce");
-console.log('Executing f: guessOnce');
+    console.log("Entering f: guessOnce");
+    console.log('\tExecuting f: guessOnce'); 
     //prompt guess and convert string input into number
     let guess = parseInt(prompt('Please guess a number between 0 and 10.'));
+    //if guess is a number, increment guessCount
+    if (typeof guess === 'number') {
+        guessCount++; console.log(`\tCurrent guess count: ${guessCount}; Data type: ${typeof guess}`);    
+    }
     //return guess to be stored and used later
     console.log(`\t\treturning guess: ${guess}\n\t\ttypeof: ${typeof guess}\n\tExiting f: guessOnce`);
     return guess;
@@ -11,8 +18,8 @@ console.log('Executing f: guessOnce');
 
 //generate target number
 let generateNum = function generateNum() {
-    console.log("\tEntering f: generateNum");
-    console.log('Executing f: generateNum');
+    console.log("Entering f: generateNum");
+    console.log('\tExecuting f: generateNum');
     //use math.random method to generate targetNum b/t 1-10
     let targetNum = (Math.floor(Math.random() * 11));
     console.log(`\t\treturning targetNum: ${targetNum}\n\t\ttypeof: ${typeof targetNum}\n\tExiting f: generateNum`);
@@ -26,13 +33,13 @@ and if guess === targetNum. If !==, call function guessOnce again for new input*
 let compare = function(fn1, fn2) {
     console.log("Entering f: compare")
     //take in guessOnce and generateNum
-    console.log(`Taking in functions: guessOnce and generateNum`);
-    console.log('Executing guessOnce()'); 
+    console.log(`Passing in functions: guessOnce and generateNum`);
+    console.log('Call f: guessOnce()'); 
     let guess = guessOnce();
-    console.log('Executing generateNum()'); 
+    console.log('Call f: generateNum()'); 
     let targetNum = generateNum();
     //compare result of guessOnce to result of generateNum
-    console.log(`Guess outside while loop: ${guess} | Target Number: ${targetNum}`);
+    console.log(`Guess value outside while loop: ${guess} | Target Number: ${targetNum}`);
     //do compare guess to num while guess !== num
     console.log("Entering do...while loop")
     while (guess !== targetNum) {
@@ -54,13 +61,12 @@ let compare = function(fn1, fn2) {
         //call guessOnce again to obtain new guess
         console.log("\t\t\tCalling guessOnce for new guess")
         guess = guessOnce();
-        console.log(`\t\t\tNew guess post-call in while loop: ${guess}`);
+        console.log(`\t\t\tUpdated guess value post-call in while loop: ${guess}`);
     }
     //if guess === targetNum, alert "Correct!"
-    alert("Correct!");
-
+    alert(`Correct! It only took you ${guessCount} guesses!`);
     console.log("Exiting f: compare");
 }
 
-//compare(guessOnce, generateNum);
+
 
